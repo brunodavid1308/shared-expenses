25a686b565cdabd588aaa30b735611e29e6f9fd6
U01A399D <brunodavid.ortega@viewnext.com>
2022-09-01 13:57:21 +0200

    feat: add basic folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpensesListComponent } from './features/sharedExpenses/delivery/ExpensesList/ExpensesList.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'expenses-list', pathMatch: 'full' },
+  { path: 'expenses-list', component: ExpensesListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { getFriendsQuery } from './features/sharedExpenses/application';
+import { ExpensesListComponent } from './features/sharedExpenses/delivery/ExpensesList/ExpensesList.component';
+import { sharedExpensesInMemoryRepository } from './features/sharedExpenses/infraestructure/SharedExpensesInMemoryRepository';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ExpensesListComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    getFriendsQuery().build({
+      repository: sharedExpensesInMemoryRepository(),
+    });
+  }
+}
