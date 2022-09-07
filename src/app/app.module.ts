803aa41c253082977d1662f5cb2dc1d8e7173c13
Bruno Ortega <bruno_o_t@hotmail.com>
2022-09-07 10:41:28 +0200

    fix: edit import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExpensesListComponent } from './features/sharedExpenses/delivery/expensesList/expensesList.component';
-import { sharedExpensesInMemoryRepository } from './features/sharedExpenses/infraestructure/sharedExpensesInMemoryRepository';
 import { ReactiveFormsModule } from '@angular/forms';
 import { sharedExpensesService } from './features/sharedExpenses/application/sharedExpensesService';
 import { sharedExpensesLocalStorageRepository } from './features/sharedExpenses/infraestructure/shareExpensesLocalStorageRepository';
+import { sharedExpensesInMemoryRepository } from './features/sharedExpenses/infraestructure/sharedExpensesInMemoryRepository';
 
 @NgModule({
   declarations: [AppComponent, ExpensesListComponent],
